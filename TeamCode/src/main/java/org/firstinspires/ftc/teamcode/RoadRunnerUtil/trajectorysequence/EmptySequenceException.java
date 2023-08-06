abe6422a994cf84e3b8391e3bdea7ca33c8f330b
R-Tacoz <roccozxyt@gmail.com>
2023-08-06 11:44:15 -0400

    import old robot, setup rr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerUtil/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.RoadRunnerUtil.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/TestBot.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Robots;
+
+public class TestBot {
+}
