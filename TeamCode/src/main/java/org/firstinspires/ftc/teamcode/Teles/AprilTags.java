dc3d225faa631ca60c593afa189788f21fa35350
R-Tacoz <roccozxyt@gmail.com>
2023-10-15 12:39:42 -0400

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/AprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/AprilTags.java
@@ -7,9 +7,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Robots.ToBeNamed;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
@@ -21,7 +24,7 @@ public class AprilTags extends LinearOpMode {
     private static final int RESOLUTION_WIDTH = 640;
     private static final int RESOLUTION_HEIGHT = 480;
     private VisionPortal visionPortal;
-    private AprilTagProcessor aprilTag;
+    private AprilTagProcessor aprilTagProcessor;
     private AprilTagDetection aprilTagDetections = null; // holds data for detected AprilTag
 
     @Override
@@ -32,7 +35,7 @@ public class AprilTags extends LinearOpMode {
 
         while(opModeIsActive()){
 
-            List<AprilTagDetection> detectedTags = aprilTag.getDetections();
+            List<AprilTagDetection> detectedTags = aprilTagProcessor.getDetections();
 
             for(AprilTagDetection detectedTag : detectedTags) {
                 if (detectedTag.metadata != null) {
@@ -47,23 +50,34 @@ public class AprilTags extends LinearOpMode {
             telemetry.update();
 
 
+            float fps = visionPortal.getFps(); // get FPS
         }
+        visionPortal.stopStreaming(); // conserve resources
+        visionPortal.resumeStreaming();
+        visionPortal.close(); // permanent stop
     }
 
     private void initAprilTag() {
         //statically create & build aprilTag
-        aprilTag = new AprilTagProcessor.Builder()
+        AprilTagLibrary tagLibrary = new AprilTagLibrary.Builder()
+                .setAllowOverwrite(true)
+                .addTags(AprilTagGameDatabase.getCurrentGameTagLibrary())
+                .addTag(10, "temp", 5, DistanceUnit.CM) // TODO: vectorfield calculations
+                .build();
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setTagLibrary(tagLibrary)
                 .build();
         if(USE_WEBCAM) {
             visionPortal = new VisionPortal.Builder()
                     .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                    .addProcessor(aprilTag)
+                    .addProcessor(aprilTagProcessor)
+                    .enableLiveView(false)
                     .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT) )
                     .build();
         }else {
             visionPortal = new VisionPortal.Builder()
                     .setCamera(BuiltinCameraDirection.BACK)
-                    .addProcessor(aprilTag)
+                    .addProcessor(aprilTagProcessor)
                     .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT) )
                     .build();
         }
