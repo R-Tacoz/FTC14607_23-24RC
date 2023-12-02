017883a1163178663117338a602768a0d9462869
Evelyn Li <evlijai22@Gmail.com>
2023-12-01 20:19:55 -0500

    crservo class import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Octonaut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Octonaut.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.Robots;
 import androidx.annotation.NonNull;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
