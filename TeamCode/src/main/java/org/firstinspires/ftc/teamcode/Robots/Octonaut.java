5a8c6a5cef17617064d9795650e9492e8923db4a
Evelyn Li <evlijai22@Gmail.com>
2023-12-02 01:08:13 -0500

    drivetrain + slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Octonaut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Octonaut.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.Robots;
 
 import androidx.annotation.NonNull;
 
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
@@ -16,8 +18,16 @@ import org.firstinspires.ftc.teamcode.util.ThreeWheelOdometry;
 public class Octonaut extends RobotBase {
     public Odometry odometry;
     public int odoTicks;
-    public CRServo tester;
+    public static CRServo tester;
     public Servo tester2;
+
+    public DcMotorEx rightSlide, leftSlide;
+    public DcMotorEx[] slides;
+    // controllers
+    public PIDFController slidepidfcontroller;
+
+    public final static int SLIDEBOTTOM = 0;
+    public final static int SLIDETOP = 960;
 
     public Octonaut(@NonNull HardwareMap hardwareMap,
         LinearOpMode opModeInstance,
@@ -39,16 +49,75 @@ public class Octonaut extends RobotBase {
 //            odoBackDist
 //        );
 
+        // claw
         tester = hardwareMap.get(CRServo.class, "tester");
-        tester2 = hardwareMap.get(Servo.class, "tester2");
 
+        //slides
+        rightSlide = hardwareMap.get(DcMotorEx.class, "RightSlide");
+        leftSlide = hardwareMap.get(DcMotorEx.class, "LeftSlide");
+        slides = new DcMotorEx[]{rightSlide, leftSlide};
+        for(DcMotorEx m: slides) {
+            m.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            m.setVelocityPIDFCoefficients(15.0, 2.0, 0.0, 0);
+            m.setPositionPIDFCoefficients(10.0);
+        }
+        resetSlideEncoders();
+
+        slidepidfcontroller = new PIDFController(50, 0.05, 0, 0);
+        //slidepidfcontroller.setIntegrationBounds(-5, 5);
+        slidepidfcontroller.setTolerance(3);
+    }
+    // ------------------------------------- MISC METHODS ------------------------------------------
+
+    /**
+     * Assigns the current position of both slide motors to tick (position) 0.
+     * MAKE SURE THE SLIDES ARE AT THE BOTTOM WHEN DOING THIS.
+     */
+    public void resetSlideEncoders() {
+        setRunMode(slides, DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        // behavior may vary between motor models, so after resetting set all slides back to RUN_TO_POSITION
+        for (DcMotorEx motor: slides) {
+            motor.setTargetPosition(motor.getCurrentPosition());
+            motor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        }
     }
 
-    public void setClawPower(double pow){
-        tester.setPower(pow);
+    // --------------------------------- MOVEMENT METHODS ------------------------------------------
+
+
+    // ------------------------------------ INTERACTOR METHODS -------------------------------------
+
+    /**
+     * Returns the slides current position
+     * @return Left slides current position ~(0-960)
+     */
+    public int getSlidePos() { return leftSlide.getCurrentPosition(); }
+
+    /**
+     * Sets the velocity of the slides
+     * @param velocity
+     */
+    public void setSlideVelo(double velocity){
+        double currentPos = getSlidePos();
+        // prevents setting velocity if it will exceed the limits
+        if ( (currentPos>=955 && velocity>0) || (currentPos<=0 && velocity<0) ) velocity = 0;
+        setRunMode(slides, DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightSlide.setVelocity(-velocity);
+        leftSlide.setVelocity(velocity);
     }
-    public void setClawPos(double pos){
-        double position = Range.clip(pos, -1, 1);
-        tester.setPower(position);
+
+    /**
+     * Extend/retract slides to desired height using encoders
+     * @param height - (ticks)
+     */
+    public void setSlidePos(int height) {
+        height = Range.clip(height, SLIDEBOTTOM, SLIDETOP);
+        int change = height - getSlidePos();
+        rightSlide.setTargetPosition(-height);
+        leftSlide.setTargetPosition(height);
+        for(DcMotorEx slide:slides) {
+            slide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            slide.setVelocity( (change>0)? 1200:1000 );
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/IdiotsInCars.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/IdiotsInCars.java
@@ -22,31 +22,137 @@ public class IdiotsInCars extends LinearOpMode {
 
     // Etc
     Octonaut robot;
+    public final DecimalFormat fourDecimals = new DecimalFormat("#.0000");
+    public final float slowSpeed = 0.3f;
+    public final float fastSpeed = 0.54f;
+    public boolean reverseDrivetrain = false;
+    public boolean movingDrivetrain = false;
+    public float lastY = 0;
+    public float lastX = 0;
+    public float lastRX = 0;
+
+    private boolean fastSlides = true;
+    private boolean movingSlide = false;
+    private byte slideDirection = 1; // direction the slides were just previously moving -1 is down, 1 is up, 0 is for set heights
+    private int lastSlidePos = 0;
+    private boolean movingToGround = false;
+
+    /**
+     * Moves the drivetrain (the four wheels). Includes strafing and rotation
+     * @param gamepad Gamepad that controls the drivetrain (gamepad1 or gamepad2)
+     * @param speedFactor Factor which the inputs are multiplied
+     * @return driveTrainPowers, an array of all the powers the drivetrain is set to (FL, BL, FR, BR)
+     */
+    public float[] moveDriveTrain(@NonNull Gamepad gamepad, @NonNegative float speedFactor) {
+        //control.drivetrain represents all 4 wheels below?
+        //control.frontLEft, backLeft, frontRight, backRight ... represent
+        float fbinput = gamepad.left_stick_y; //either input is analog of digital based on which piece of gamepad is pressed
+        if(gamepad.dpad_up) fbinput = 1;
+        else if(gamepad.dpad_down) fbinput = -1;
+
+        float y = -fbinput * speedFactor;
+        float x = gamepad.left_stick_x * speedFactor * 1.75f;
+        float rx = gamepad.right_stick_x * speedFactor * 0.6f;
+
+        // brake using setVelocity
+        if(y == 0 && x == 0 && rx == 0) {
+            for (DcMotorEx m: robot.drivetrain) {
+                m.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                m.setVelocity(0);
+            }
+            return new float[]{0,0,0,0};
+        } else {
+            for (DcMotorEx m : robot.drivetrain) m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            float frontLeftPower = (y + x + rx);
+            float backLeftPower = (y - x + rx) * 1.17f;
+            float frontRightPower = (y - x - rx);
+            float backRightPower = (y + x - rx) * 1.15f;
+            boolean braking = false;
+
+            float[] driveTrainPowers = new float[]{
+                    Float.parseFloat(fourDecimals.format((double) frontLeftPower)),
+                    Float.parseFloat(fourDecimals.format((double) backLeftPower)),
+                    Float.parseFloat(fourDecimals.format((double) frontRightPower)),
+                    Float.parseFloat(fourDecimals.format((double) backRightPower)),
+            };
+            robot.frontLeft.setPower(frontLeftPower);
+            robot.backLeft.setPower(backLeftPower);
+            robot.frontRight.setPower(frontRightPower);
+            robot.backRight.setPower(backRightPower);
+
+            return driveTrainPowers;
+        }
+    }
+
+    /**
+     * Moves the slides
+     * @param gamepad Gamepad that controls the slides (gamepad1 or gamepad2)
+     * @return The position of the slide in ticks ~[0,960]
+     */
+    public int moveSlides(@NonNull Gamepad gamepad) {
+        int slidePos = robot.getSlidePos();
+        double up = -gamepad.right_stick_y;
+        double down = gamepad.right_stick_y;
+
+        if(gamepad.dpad_up) fastSlides = true;
+        else if(gamepad.dpad_down) fastSlides = false;
+        int SLIDE_UP_VELO = (int)(1000 * (fastSlides ? 1:0.6));
+        int SLIDE_DOWN_VELO = (int)(-800 * (fastSlides ? 1:0.45));
+        // joystick: continuous
+        if (up>0 || down>0) {
+            if(up>0 && slidePos >= Octonaut.SLIDETOP-30) robot.setSlidePos(Octonaut.SLIDETOP);
+            else if(down>0 && slidePos <= Octonaut.SLIDEBOTTOM+30) robot.setSlidePos(Octonaut.SLIDEBOTTOM);
+            else {
+                movingSlide = true;
+                double velo = 0;
+                if (up > 0) { // ticks/s
+                    velo = SLIDE_UP_VELO; slideDirection = 1; }
+                else {
+                    velo = SLIDE_DOWN_VELO; slideDirection = -1; }
+                robot.setSlideVelo(velo);
+            }
+            // a,b,x,y, preset positions or hold current position
+        } else {
+            if (movingSlide) {
+                movingSlide = false;
+                lastSlidePos = slidePos + 5*slideDirection;
+            }
+            // stop moving to ground if reached it
+            if (slidePos < 20) movingToGround = false;
+
+        }
+        return slidePos;
+    }
 
     //test servo (11/30/2023)
     private double servoPow = 0.5;
     private double servoPos = 0.666;
     public void moveClaw(@NonNull Gamepad gamepad) {
         if (gamepad.left_bumper && gamepad.right_bumper) servoPow = 0.5;
-        else if (gamepad.right_bumper) servoPow += 0.01;
-        else if (gamepad.left_bumper) servoPow -= 0.01;
-        robot.setClawPower(servoPow);
-        if (gamepad.b) {
-            robot.setClawPos(servoPos);
-        }
+        else if (gamepad.right_bumper) servoPow = 1;
+        else if (gamepad.left_bumper) servoPow = 0;
+        Octonaut.tester.setPower(servoPow);
     }
 
     public void runOpMode() {
         robot = new Octonaut(hardwareMap,this, 0,0, 8192, 5, 30, 0);
+        float speedFactor = fastSpeed;
+
+        for(DcMotorEx m:robot.drivetrain) m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
         while(opModeIsActive())
         {
-//            telemetry.addData("imu", robot.imu.getAngularOrientation());
+            telemetry.addData("imu", robot.imu.getAngularOrientation());
 //            telemetry.addData("context", robot.odometry.getContext());
-            telemetry.update();
+            float[] driveTrainPowers = moveDriveTrain(gamepad1, speedFactor);
             moveClaw(gamepad1);
+
+            telemetry.addData("Speed factor", speedFactor);
+            telemetry.addData("Drivetrain powers", Arrays.toString(driveTrainPowers));
             telemetry.addData("Servo power: ", servoPow);
+            telemetry.update();
         }
     }
 }
