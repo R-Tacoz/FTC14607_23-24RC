e7577b229199faa8cd4c634235b79466ef613184
R-Tacoz <roccozxyt@gmail.com>
2023-12-10 11:27:05 -0500

    prop detect things, needs testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/vision/PropDetectProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/vision/PropDetectProcessor.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode.util.vision;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -18,22 +19,44 @@ import org.opencv.imgproc.Imgproc;
  */
 public class PropDetectProcessor implements VisionProcessor {
 
-    public enum PropPosition {
-        NONE,
-        LEFT,
-        CENTER,
-        RIGHT,
-    }
+    public static final double DETECTION_THRESHOLD = 0.2;
+    public static final int
+            NO_DETECTION = -1,
+            LEFT_DETECTION = 0,
+            CENTER_DETECTION = 1,
+            RIGHT_DETECTION = 2;
+    public static final int
+            RED_DETECTION = 0,
+            BLUE_DETECTION = 1;
+    private volatile int detectedPosition = NO_DETECTION;
+    private volatile int detectedColor = RED_DETECTION;
+    private volatile double detectionConfidence = 0;
 
-    private volatile PropPosition detectedPosition = PropPosition.NONE; //position with the most amount of the color
-    private double detectionConfidence = 0.00;
-
-    // Color boundaries for masking. Adjust as needed.
+    // Color boundaries for masking. Adjust as needed. In BGR (not RGB)
     private static final Scalar
-            RED_LOWER_BOUNDS = new Scalar(155, 55, 55),
-            RED_UPPER_BOUNDS = new Scalar(179, 225, 255),
-            BLUE_LOWER_BOUNDS = new Scalar(100, 0, 0),
-            BLUE_UPPER_BOUNDS = new Scalar(255,50,50);
+            RED = new Scalar(0, 0, 255),
+            BLUE = new Scalar(255, 0, 0),
+            RED_LOWER_BOUNDS =  new Scalar(0  , 255, 100),
+            RED_UPPER_BOUNDS =  new Scalar(176 , 224 , 255), // TODO: red improper clipping
+            BLUE_LOWER_BOUNDS = new Scalar(120, 255, 100),
+            BLUE_UPPER_BOUNDS = new Scalar(114, 192, 255),
+            RED_LOWER_LBOUNDS =  new Scalar(0  , 224, 100),
+            RED_UPPER_LBOUNDS =  new Scalar(15 , 255 , 255),
+            RED_LOWER_RBOUNDS =  new Scalar(165  , 224, 100),
+            RED_UPPER_RBOUNDS =  new Scalar(179 , 255 , 255);
+//            BLUE_LOWER_BOUNDS = new Scalar(80, 152, 100),
+//            BLUE_UPPER_BOUNDS = new Scalar(180, 255, 255);
+    // vvv in BGR vvv
+//            RED_LOWER_BOUNDS =  new Scalar(0  , 0  , 100),
+//            RED_UPPER_BOUNDS =  new Scalar(63 , 31 , 255),
+//            BLUE_LOWER_BOUNDS = new Scalar(100, 0  , 0),
+//            BLUE_UPPER_BOUNDS = new Scalar(255, 100, 63);
+    // old
+//            RED_LOWER_BOUNDS = new Scalar(155, 55, 55),
+//            RED_UPPER_BOUNDS = new Scalar(179, 225, 255),
+//            BLUE_LOWER_BOUNDS = new Scalar(100, 0, 0),
+//            BLUE_UPPER_BOUNDS = new Scalar(255,50,50);
+
 
     // Regions to color mask; values are proportions of the screen. Adjust as needed.
     private static final double
@@ -46,29 +69,20 @@ public class PropDetectProcessor implements VisionProcessor {
             RIGHT_TLCORNER_X    = 0.66, RIGHT_TLCORNER_Y    = 0,
             RIGHT_WIDTH         = 0.33, RIGHT_HEIGHT        = 1;
 
-    private Rect leftBounds, centerBounds, rightBounds, fullBounds;
-    private double[] regionSizes; // # pixels in each region
-
-    // Indices 0,1,2 correspond to left, center, right
+    private Rect fullBounds;
     private Rect[] regionBounds;
-    private double[] confidences;
-    private double leftPercent, centerPercent, rightPercent;
+    private double[] regionSizes; // # pixels in each region
+    private final double[][] confidences = new double[2][3];
 
     private int frameWidth, frameHeight;
-    private Mat currentInput, blurred;
-    private Mat
-            leftRegion, centerRegion, rightRegion,
-            leftMasked, centerMasked, rightMasked;
-    private Mat[] regions;
-    private Mat[] masked;
+    private volatile Mat currentInput, blurred;
+    private final Mat[] regions = new Mat[3]; // portions of the frame
+    private final Mat[][] masked = new Mat[2][3]; // regions, color masked
 
     @Override
     public void init(int width, int height, CameraCalibration calibration){
         frameWidth = width;
         frameHeight = height;
-        confidences = new double[] {0, 0, 0};
-        regions = new Mat[] {null, null, null};
-        masked = new Mat[] {null, null, null};
         regionBounds = new Rect[] {
                 new Rect(
                         (int) (LEFT_TLCORNER_X * frameWidth), (int) (LEFT_TLCORNER_Y * frameHeight),
@@ -90,7 +104,8 @@ public class PropDetectProcessor implements VisionProcessor {
     @Override
     public Mat processFrame(Mat input, long captureTimeNanos) {
         // Frame preprocessing: RGB to HSV + normalized Box Filter Blur
-        Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2HSV); // TODO: u sure its not R-G-B
+        currentInput = input;
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2HSV); // TODO: try without convert
         Imgproc.blur(input, blurred, new Size(frameWidth, frameHeight));
         blurred = blurred.submat(fullBounds);
 
@@ -100,76 +115,65 @@ public class PropDetectProcessor implements VisionProcessor {
             regions[i] = blurred.submat( regionBounds[i] );
 
             // color mask it & store
-            Core.inRange( regions[i], RED_LOWER_BOUNDS, RED_UPPER_BOUNDS, masked[i] );
+            Mat redMaskedL = new Mat(), redMaskedR = new Mat();
+            Core.inRange( regions[i], RED_LOWER_BOUNDS, RED_UPPER_BOUNDS, masked[0][i] );
+//            Core.inRange( regions[i], RED_LOWER_LBOUNDS, RED_UPPER_LBOUNDS, redMaskedL );
+//            Core.inRange( regions[i], RED_LOWER_RBOUNDS, RED_UPPER_RBOUNDS, redMaskedR );
+//            Core.add(redMaskedL, redMaskedR, masked[0][i]);
+
+            Core.inRange( regions[i], BLUE_LOWER_BOUNDS, BLUE_UPPER_BOUNDS, masked[1][i] );
 
             // get confidence percentage (proportion of pixels that were not masked out) & store
-            confidences[i] = Core.countNonZero(masked[i]) / regionSizes[i];
-        }
-//        leftRegion = input.submat(leftBounds);
-//        centerRegion = input.submat(centerBounds);
-//        rightRegion = input.submat(rightBounds);
-//
-//        Core.inRange(leftRegion, RED_LOWER_BOUNDS, RED_UPPER_BOUNDS, leftMasked);
-//        Core.inRange(centerRegion, RED_LOWER_BOUNDS, RED_UPPER_BOUNDS, centerMasked);
-//        Core.inRange(rightRegion, RED_LOWER_BOUNDS, RED_UPPER_BOUNDS, rightMasked);
-//
-//        double total_pixels = frameWidth * frameHeight; // TODO: this isn't even right
-//        leftPercent = Core.countNonZero(leftMasked) / total_pixels;
-//        centerPercent = Core.countNonZero(centerMasked) / total_pixels;
-//        rightPercent = Core.countNonZero(rightMasked) / total_pixels;
-
-        double maxPercent = leftPercent == 0 && centerPercent == 0 && rightPercent == 0 ? -1 : Math.max(leftPercent, Math.max(centerPercent, rightPercent));
-
-        detectionConfidence = maxPercent;
-
-        if(maxPercent == leftPercent){
-            detectedPosition = PropPosition.LEFT;
-            Imgproc.rectangle(
-                    input,
-                    new Point(0,0),
-                    new Point(TeamVisionUtilities.CAMERA_RESOLUTION_WIDTH / 3d, TeamVisionUtilities.CAMERA_RESOLUTION_HEIGHT),
-                    RED_LOWER_BOUNDS);
-        }
-        else if(maxPercent == centerPercent){
-            detectedPosition = PropPosition.CENTER;
-        }
-        else if(maxPercent == rightPercent){
-            detectedPosition = PropPosition.RIGHT;
-        }
-        else{
-            detectedPosition = PropPosition.NONE;
+            confidences[0][i] = Core.countNonZero(masked[0][i]) / regionSizes[i];
+            confidences[1][i] = Core.countNonZero((masked[1][i])) / regionSizes[i];
         }
 
+        // get the most confident detection
+        double maxConfidence = 0;
+        int maxColor = -1, maxPos = -1;
+        for (int color = 0; color < 1; color++) {
+            for (int pos = 0; pos < 2; pos ++) {
+                if (confidences[color][pos] > maxConfidence) {
+                    maxConfidence = confidences[color][pos];
+                    maxColor = color;
+                    maxPos = pos;
+                }
+            }
+        }
 
-        currentInput = input;
+        detectedColor = maxColor;
+        detectedPosition = maxPos;
+        detectionConfidence = maxConfidence;
+        if (detectionConfidence < DETECTION_THRESHOLD) {
+            detectedPosition = NO_DETECTION;
+            return blurred;
+        } else {
+            Scalar color = (detectedColor == 0) ? RED:BLUE;
+            Imgproc.rectangle(input, regionBounds[detectedPosition], color);
+        }
 
-        //stuff
         return blurred;
     }
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-        
+        canvas.drawRect(0, 0, 100, 100, new Paint(Color.MAGENTA));
     }
 
-    public PropPosition getPropPosition() {
+    public int getDetectedPosition() {
         return detectedPosition;
     }
-
-    public double getPropPercent(){
+    public int getDetectedColor() {
+        return detectedColor;
+    }
+    public double getDetectionConfidence(){
         return detectionConfidence;
     }
-
     public Mat getCurrentInput(){
         return currentInput;
     }
+    public Mat getBlurred(){
+        return blurred;
+    }
 
-    public Mat getLeftRegion(){ return leftRegion; }
-    public Mat getCenterRegion(){ return centerRegion; }
-    public Mat getRightRegion(){ return rightRegion; }
-    public Mat getBlurred(){ return blurred;}
-
-    public Mat getLeftMasked() { return leftMasked; }
-    public Mat getCenterMasked() { return centerMasked; }
-    public Mat getRightMasked() { return rightMasked; }
 }
