dee0c3d1ffe580e8467aa5d79934bd38c6611c97
Devin Park <devinpark345@gmail.com>
2024-01-07 13:14:04 -0500

    Outreach Robot and Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/OutreachTeleop.java
@@ -0,0 +1,89 @@
+package org.firstinspires.ftc.teamcode.teleops;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
+
+import org.checkerframework.checker.index.qual.NonNegative;
+import org.firstinspires.ftc.teamcode.robots.Octonaut;
+import org.firstinspires.ftc.teamcode.robots.Outreach;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+@TeleOp(name = "Outreach Teleop", group = "Main")
+public class OutreachTeleop extends LinearOpMode {
+    Outreach robot;
+    public final DecimalFormat fourDecimals = new DecimalFormat("#.0000");
+
+    private int lastSlidePos = 0;
+
+    public float[] moveDriveTrain(@NonNull Gamepad gamepad, @NonNegative float speedFactor) {
+        float FBinput = gamepad.left_stick_y;
+        if(gamepad.dpad_up) FBinput = 1;
+        else if(gamepad.dpad_down) FBinput = -1;
+
+        float y = -FBinput * speedFactor;
+        float x = gamepad.left_stick_x * speedFactor;
+        float rx = gamepad.right_stick_x * speedFactor;
+
+        for (DcMotorEx m : robot.drivetrain) m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        float frontLeftPower = (y + x + rx);
+        float backLeftPower = (y - x + rx) * 1.1f;
+        float frontRightPower = (y - x - rx);
+        float backRightPower = (y + x - rx) * 1.1f;
+
+        float[] driveTrainPowers = new float[]{
+                Float.parseFloat(fourDecimals.format(frontLeftPower)),
+                Float.parseFloat(fourDecimals.format(backLeftPower)),
+                Float.parseFloat(fourDecimals.format(frontRightPower)),
+                Float.parseFloat(fourDecimals.format(backRightPower)),
+        };
+
+        robot.frontLeft.setPower(frontLeftPower);
+        robot.backLeft.setPower(backLeftPower);
+        robot.frontRight.setPower(frontRightPower);
+        robot.backRight.setPower(backRightPower);
+
+        return driveTrainPowers;
+    }
+
+    public int moveSlides(@NonNull Gamepad gamepad) {
+        int slidePos = robot.getSlidePos();
+        int setPos = slidePos;
+        if (gamepad.b) {setPos = 300;}
+        else if (gamepad.a) {setPos = Octonaut.GROUND; }
+        setPos = Range.clip(setPos, Octonaut.SLIDEBOTTOM, Octonaut.SLIDETOP);
+        robot.setSlidePos(setPos);
+
+        slidePos = robot.getSlidePos();
+        return slidePos;
+    }
+
+    public void runOpMode() {
+        robot = new Outreach(this);
+        float speedFactor = 1f;
+
+        for(DcMotorEx m:robot.drivetrain) m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        while(opModeIsActive())
+        {
+            telemetry.addData("imu", robot.imu.getAngularOrientation());
+//            telemetry.addData("context", robot.odometry.getContext());
+            float[] driveTrainPowers = moveDriveTrain(gamepad1, speedFactor);
+            int slidePos = moveSlides(gamepad2);
+
+            telemetry.addData("Speed factor", speedFactor);
+            telemetry.addData("Drivetrain powers", Arrays.toString(driveTrainPowers));
+            telemetry.addData("Slide position", slidePos);
+            telemetry.update();
+        }
+    }
+}
